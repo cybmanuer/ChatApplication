a04c134242aa18be8467153d465a9a315b6ffaef
cybmanuer <manoharmanu1722003@gmail.com>
2025-08-17 16:53:12 +0530

    updated the package JSON files

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,7 +39,7 @@ app.use("/api/messages",messageRoutes);  // message related code  -> logic is de
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, '../frontend/dist'))); // serve static files from the build folder\\
     app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   }); // the code is for production environment when deploying to production
 }
 
